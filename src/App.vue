8e07350ee5ae0aa5bbe1f41bf0df5ddbb0b41ca9
EvyatarHaim1 <evyataros1@gmail.com>
2022-06-20 20:32:30 +0300

    clean up & adding api key of weather api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">Hola</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data : () => {
+    return {
+      api_key: "3730fe48686b8419cdc646247a846425"
+    }
   }
 }
 </script>
@@ -17,10 +17,5 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
