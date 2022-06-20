e41072790a5345b3b70bcdedaadd23e908b547d8
EvyatarHaim1 <evyataros1@gmail.com>
2022-06-20 21:10:30 +0300

    adding a background image and an ipnut field

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,120 @@
 <template>
-  <div id="app">Hola</div>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northamptom, UK</div>
+          <div class="date">Monday 20 January 2022</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°C</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data : () => {
+  name: "App",
+  data: () => {
     return {
-      api_key: "3730fe48686b8419cdc646247a846425"
-    }
-  }
-}
+      api_key: "3730fe48686b8419cdc646247a846425",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrate", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+background-image: url("./assets/bg.gif");
+background-size: cover;
+background-position: bottom;
+transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding:25px;
+  background-image: linear-gradient(to bottom, rgvba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+.search-box {
+ width: 100%;
+ margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size:20px;
+
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.1);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0,0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.wearher-box {
+  text-align: center;
+}
+
+.wearher-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: white;
+  font-size:102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
+  background-color:rgba(255,255,255,0.25);
+  border-radius: 16xp;
+  margin: 30px 0px;
+  
+  box-shadow: 3px 6px rgba(0,0,0,0.25);
 }
 </style>
